a952561e2693c93654ea11b0b7420558bbdc6d06
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-10-22 11:21:06 +0530

    added DNS USP Simulator

---
+++ DNSSim/Client.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    DatagramSocket clientsocket = new DatagramSocket();
+    InetAddress ipaddress;
+    int portaddr = 1362;
+
+    if (args.length == 0)
+      ipaddress = InetAddress.getLocalHost();
+    else
+      ipaddress = InetAddress.getByName(args[0]);
+
+    byte[] senddata = new byte[1024];
+    byte[] receivedata = new byte[1024];
+
+    System.out.print("Enter the hostname: ");
+    String sentence = br.readLine();
+    senddata = sentence.getBytes();
+
+    DatagramPacket pack = new DatagramPacket(senddata, senddata.length, ipaddress, portaddr);
+    clientsocket.send(pack);
+
+    DatagramPacket recvpack = new DatagramPacket(receivedata,receivedata.length);
+    clientsocket.receive(recvpack);
+
+    String modified = new String(recvpack.getData());
+    System.out.println("IP Address: " + modified);
+    clientsocket.close();
+  }
+}
