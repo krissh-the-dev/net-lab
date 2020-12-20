918383fe9b7cde0838c2cf2a2a19b20bd86f4b4b
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-12-20 16:07:28 +0000

    added html file download functionality to SocketClient

--- SocketHTTPClient/SocketHTTPClient.java
+++ SocketHTTPClient/SocketHTTPClient.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.io.*;
 import java.net.*;
 
@@ -10,23 +9,36 @@ public class SocketHTTPClient {
   public static void main(String[] args) {
     String hostName = "www.google.com";
     int portNumber = 80;
+    Boolean startDownload = false;
 
     try {
       Socket socket = new Socket(hostName, portNumber);
-      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      PrintWriter socketOutStream = new PrintWriter(socket.getOutputStream(), true);
       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      out.println("GET / HTTP/1.1\nHost: www.google.com\n\n");
-      String inputLine;
-      while ((inputLine = in.readLine()) != null) {
-        System.out.println(inputLine);
+      BufferedWriter writer = new BufferedWriter(new FileWriter("download.html"));
+
+      socketOutStream.println("GET / HTTP/1.1\nHost: www.google.com\n\n");
+      String line;
+      while ((line = in.readLine()) != null) {
+        if (line.contains("<!")) {
+          System.out.println("Starting download...");
+          startDownload = true;
+        }
+        if (startDownload) {
+          writer.write(line);
+        } else {
+          System.out.println(line);
+        }
       }
-      return;
+
+      writer.close();
+      socket.close();
     } catch (UnknownHostException e) {
-        System.err.println("Don't know about host " + hostName);
-        System.exit(1);
+      System.err.println("Don't know about host " + hostName);
+      System.exit(1);
     } catch (IOException e) {
-        System.err.println("Couldn't get I/O for the connection to " + hostName);
-        System.exit(1);
+      System.err.println("Couldn't get I/O for the connection to " + hostName);
+      System.exit(-1);
     }
   }
 }
